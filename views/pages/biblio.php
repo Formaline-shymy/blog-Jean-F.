f9993782c1fb6e51728b680b1f5288c24575df47
Formaline-shymy <52758074+Formaline-shymy@users.noreply.github.com>
2020-03-12 18:37:58 +0100

    Delete biblio.php

--- views/pages/biblio.php
+++
@@ -1,4 +0,0 @@
-<?php require APPROOT . '/views/inc/header.php'; ?>
-  <h1><?php echo $data['title']; ?></h1>
-  <p><strong><?php echo $data['description']; ?></strong></p>
-<?php require APPROOT . '/views/inc/footer.php'; ?>
