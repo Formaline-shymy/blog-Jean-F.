a38a74a156ce40fea0af17c30beb14a381249712
Formaline-shymy <52758074+Formaline-shymy@users.noreply.github.com>
2020-03-17 21:02:38 +0100

    Add files via upload

---
+++ Untitled-1.php
@@ -0,0 +1,14 @@
+<div class="container">
+    <div id="full-header">
+        <div id="pre-header">
+            <a class="link-newsletter" href="/newsletter">
+                <i class="visible-xs-inline-block fa fa-envelope"></i>
+                <span class="hidden-xs">
+                    S'inscrire à la newslette</span>
+            </a>
+            <span class="social-links hidden-xs">
+                <a href=""#" target="_blank" ><i class="fab fa-instagram"></i><span class="sr-only">Instagram</span></a>
+                <a href="#" target="_blank" ><i class="fab fa-twitter"></i><span class="sr-only">Twitter</span></a>
+                <a href="#" target="_blank" ><i class="fab fa-facebook-f"></i><span class="sr-only">Facebook</span></a>
+            </span>
+          
