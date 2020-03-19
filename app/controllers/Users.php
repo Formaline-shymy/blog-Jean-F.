b5332e55304fa59e4c607625ab70e0287333117c
Formaline-shymy <52758074+Formaline-shymy@users.noreply.github.com>
2020-03-19 15:57:50 +0100

    add login/logout function in admin panel

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -175,4 +175,12 @@
                 header("Location: ../admins/index");
                 
             }
+
+            public function logout(){
+                unset($_SESSION['user_id']);
+                unset($_SESSION['user_email']);
+                unset($_SESSION['user_name']);
+                session_destroy();
+                header("Location: ../users/login");
+            }
     }
--- app/views/admins/index.php
+++ app/views/admins/index.php
@@ -1,17 +1,35 @@
-<?php require APPROOT . '/views/inc/adminheader.php'; ?>
-  
-    <nav id="navbar">
-        <header>Tableau bord</header>
-    <hr>
-        <ul>
-          <li><a class="nav-link" href="#Administration des posts">Administration</a></li>
-          <li><a class="nav-link" href="#Moderation des commentaires">Moderation</a></li>
-          <li><a class="nav-link" href="#Autres chose">Autres chose</a></li>
-        </ul>
-
+  <?php require APPROOT . '/views/inc/adminheader.php'; ?>
+    <nav class="topnav">
+      <div class="topnav-right">
+          <?php if (isset ($_SESSION['user_id'])) :?>
+            <a class = "link" href="<?php echo URLROOT; ?>/pages/index">Se déconnecter</a>
+          <?php endif;?>
+          <a class = "link" href="<?php echo URLROOT; ?>/index.php">Changer le mot de passe</a>
+      </div>
     </nav>
-      <main id="main-doc">
-        <section class="main-section" id="table">
-          <header>Profil d'administrateur</header>
-          <code><i class="fas fa-user"> <br> Bonjour </code>
-        </section>
+
+    <nav id="navbar2">
+      <table class="table">
+          <tbody>
+            <tr>
+              <td><a class="nav-link" href="#Tableau de bord">Tableau de bord</a></td>
+            </tr>
+            <tr>
+              <td><a class="nav-link" href="#Administration des posts">Administration</a></td>
+            </tr>
+            <tr>
+              <td><a class="nav-link" href="#Moderation des commentaires">Moderation</a></td>
+            </tr>
+            <tr>
+              <td><a class="nav-link" href="#Autres chose">Autres chose</a></td>
+            </tr>
+          </tbody>    
+      </table>
+    </nav>
+
+    <main id="main-doc">
+      <div class ="container id=table ">
+        <header>Profil d'administrateur </header>
+        <p> Bienvenue</p>
+      </div>
+    </main>
