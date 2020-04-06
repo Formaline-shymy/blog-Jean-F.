731f80675644360cbd755f2bc37b852b2c961b3e
Formaline-shymy <52758074+Formaline-shymy@users.noreply.github.com>
2020-04-06 23:03:04 +0200

    comments

---
+++ views/adminposts/delete.php
@@ -0,0 +1,44 @@
+<?php require APPROOT . '/views/inc/adminheader.php'; ?>
+    <main id="main-doc">  
+
+    <h2 class="text-uppercase text-center text-white mt-0 mb-3"> Supprimer</h2>
+        <form action="<?php echo URLROOT; ?>/adminposts/delete/<?php echo $data['post_id']; ?>" method="post">
+            <div class="card card-body bg-light mt-2">
+                <div class="row mx-auto mb-2">
+                  <div class="form-group mr-5">
+                    <label for="title">Titre: <sup>*</sup></label>
+                    <input type="text" name="title" class="form-control form-control-lg <?php echo (!empty($data['title_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['title']; ?>">
+                    <span class="invalid-feedback"><?php echo $data['title_err']; ?></span>
+                  </div>
+                  <div class="form-group mr-5">
+                    <label for="date">Date: <sup>*</sup></label>
+                    <input type="date" name="date" class="form-control form-control-lg" value="<?php echo $data['date']; ?>">
+                  </div>
+                      <br><br>
+            </div>     
+            <div class="form-group">
+                   <label for="body">Contenu: <sup>*</sup></label>
+                   <textarea id="mytextarea" name="chapter" form-control form-control-lg <?php echo (!empty($data['chapter_err'])) ? 'is-invalid' : ''; ?>"><?php echo $data['chapter']; ?></textarea>
+                    <span class="invalid-feedback">
+                    <?php echo $data['chapter_err']; ?></span>
+            </div>              
+                  <div class="row">
+                      <div class="col">
+                       <input type="submit" class="btn btn-info btn-block mt-3" value="Supprimer">                 
+                      </div>
+                  </div>
+            </div> 
+        </form>     
+    </main>
+
+
+
+
+
+
+      
+        
+
+
+
+
---
+++ views/inc/navbar.php
@@ -0,0 +1,16 @@
+<navbar>
+  <div id="mySidenav" class="sidenav">
+      <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+      <a href="<?php echo URLROOT; ?>">Accueil</a>
+      <a href= "<?php echo URLROOT; ?>/pages/about">Biographie</a>
+      <a href="<?php echo URLROOT; ?>/posts/index">Lire des chapitres de Billet simple pour l'Alaska</a>
+      <a href="<?php echo URLROOT; ?>/pages/biblio">Romans</a>
+      <!-- <a href="<?php echo URLROOT; ?>/users/register">Inscription</a> -->
+      <a href="<?php echo URLROOT; ?>/users/login">Connexion</a>
+  </div>
+
+  <div id="main">
+      <span style="font-size:30px; color: white; cursor:pointer" onclick="openNav()">&#9776;&nbsp;</span>
+      <a class="navbar-brand" href="<?php echo URLROOT; ?>"><?php echo SITENAME;?></a>
+ </div>
+</navbar>
